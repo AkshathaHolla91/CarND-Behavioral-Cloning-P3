6e4c1dd85277e60c65936652efb78a16f5e959e7
Akshatha Holla <Akshatha_Holla@ad.infosys.com>
2018-02-26 16:57:05 +0530

    First Commit

---
+++ model.py
@@ -0,0 +1,77 @@
+import csv
+import cv2
+import numpy as np
+import os
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Dropout, Activation
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+ 
+lines=[]
+with open("./Data/driving_log.csv") as csvfile:
+    reader=csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+#os.listdir(".")
+#print(len(lines))   
+lines=lines[1:]
+images=[]
+measurements=[]
+
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                name = './IMG/'+batch_sample[0].split('/')[-1]
+                center_image = cv2.imread(name)
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                angles.append(center_angle)
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
+
+ch, row, col = 3, 80, 320  # Trimmed image format
+# for line in lines:
+#     source_path=line[0]
+#     filename=source_path.split('/')[-1]
+#     current_path='./Data/IMG/'+filename
+#     image=cv2.imread(current_path)
+#     images.append(image)
+#     #print(line[3])
+#     measurement=float(line[3])
+#     measurements.append(measurement)
+          
+# X_train=np.array(images)
+# y_train=np.array(measurements)
+#print(np.shape(X_train))
+#print(y_train)
+model=Sequential()
+model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Convolution2D(6, 5, 5, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Dropout(0.5))
+model.add(Convolution2D(6, 5, 5, activation='relu'))
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
+model.add(Dense(1))
+
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+model.save("model.h5")
