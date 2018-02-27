82d1da48b33f2b1e8ee13f9dd6c5b0ed70a53810
Akshathaholla91 <akshathaholla91@gmail.com>
2018-02-27 08:13:58 +0000

    Nvidia model

--- model.py
+++ model.py
@@ -82,6 +82,9 @@ ch, row, col = 3, 80, 320  # Trimmed image format
 #print(y_train)
 model=Sequential()
 model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
+
+# Comma.ai model
+
 # model.add(Lambda(lambda x: x/255.0 - 0.5))
 # model.add(Convolution2D(16, 8, 8, subsample=(4, 4), border_mode="same"))
 # model.add(ELU())
@@ -96,17 +99,30 @@ model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
 # model.add(ELU())
 # model.add(Dense(1))
 
+model.add(Lambda(lambda x: x/255.0 - 0.5))
+
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(48, 3, 3, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+
 #Lenet model with 15 epochs
 
-model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
-model.add(Convolution2D(6, 5, 5, activation='relu'))
-model.add(MaxPooling2D())
-model.add(Dropout(0.5))
-model.add(Convolution2D(6, 5, 5, activation='relu'))
-model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
-model.add(Dense(1))
+# model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
+# model.add(Convolution2D(6, 5, 5, activation='relu'))
+# model.add(MaxPooling2D())
+# model.add(Dropout(0.5))
+# model.add(Convolution2D(6, 5, 5, activation='relu'))
+# model.add(Flatten())
+# model.add(Dense(120))
+# model.add(Dense(84))
+# model.add(Dense(1))
 
 
 model.compile(loss='mse', optimizer='adam')
